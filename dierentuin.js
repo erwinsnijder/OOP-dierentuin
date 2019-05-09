d272004aa499bb3e1b215817d17324b3a2036e6d
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-09 16:47:35 +0200

    Update dierentuin.js

--- dierentuin.js
+++ dierentuin.js
@@ -1,17 +1,28 @@
 let fish = {
   breathes: "water",
-  skin: "scales"
+  skin: "scales",
+  class: "animal"
 };
 console.log("a fish breathes " + fish.breathes + " and a fish has " + fish.skin);
 
 let mamal = {
   breathes: "air",
-  skin: "fur"
+  skin: "fur",
+  class: "animal"
 };
 console.log("a mamal breathes " + mamal.breathes + " and a mamal has " + mamal.skin);
 
 let bird= {
   breathes: "air",
-  skin: "feathers"
+  skin: "feathers",
+  class: "animal"
 };
-console.log("bird breathes " + bird.breathes + " and a bird has " + bird.skin);
+console.log("a bird breathes " + bird.breathes + " and a bird has " + bird.skin);
+
+console.log(" a fish is an "+ fish.class + "," + " a mamal is an "+ mamal.class + "," + " a bird is an "+ bird.class);
+ let caretaker= {
+name: "garry",
+  care: "animals"
+
+ };
+ console.log (caretaker.name + "takes care of" + caretaker.care);
