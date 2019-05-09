f1c2e2279a8a01e51fd9d290bea4cc6964967a33
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-09 16:10:23 +0200

    Initial commit

---
+++ dierentuin.js
@@ -0,0 +1,17 @@
+let fish = {
+  breathes: "water",
+  skin: "scales"
+};
+console.log(fish.skin);
+
+let mamal = {
+  breathes: "air",
+  skin: "fur"
+};
+console.log(mamal.skin);
+
+let bird= {
+  breathes: "air",
+  skin: "feathers"
+};
+console.log(bird.skin);
